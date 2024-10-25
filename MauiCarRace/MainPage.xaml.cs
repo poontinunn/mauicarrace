14eef04acee293670b23a201d8e1a4c8cf396243
poontinun <mypoonplys@MacBook-Pro-khxng-poontinun.local>
2024-10-25 20:43:56 +0700

    Added all project files

---
+++ MauiCarRace/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MauiCarRace.Classes;
+
+namespace MauiCarRace;
+
+public partial class MainPage : ContentPage
+{
+	Car car1;
+	Car car2;
+
+	bool finish = false;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		this.car1 = new Car("Farrari", 20);
+		this.car2 = new Car("Porsche", 10);
+		car1Info.Text = car1.GetCarInfo();
+		car2Info.Text = car2.GetCarInfo();
+	}
+
+
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		//double distance = 2000;
+		var distance = Application.Current.MainPage.Width; // Get the screen width as distance
+
+		if (!finish)
+		{
+			var timeUseCar1 = this.car1.RunForDistance(distance);
+			ImageCar1.TranslateTo(distance - 100, 0, timeUseCar1 * 10, Easing.Linear);
+			car1Info.Text = car1.GetCarInfo();
+			timeUsecar1Info.Text = $"{this.car1.Name}  time use : {timeUseCar1}";
+
+			var timeUseCar2 = this.car2.RunForDistance(distance);
+			ImageCar2.TranslateTo(distance - 100, 0, timeUseCar2 * 10, Easing.Linear);
+			car2Info.Text = car2.GetCarInfo();
+			timeUsecar2Info.Text = $"{this.car2.Name}  time use : {timeUseCar2}";
+			finish = true;
+		}
+		else
+		{
+			ImageCar1.TranslateTo(0, 0, 0, Easing.Linear);
+			ImageCar2.TranslateTo(0, 0, 0, Easing.Linear);
+			finish = false;
+		}
+	}
+}
+
